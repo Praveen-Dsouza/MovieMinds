44fa0bb3a9a5073a9f7089a0a58ed9d8897e4ea6
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-09-16 11:58:40 +0530

    sign in/up form

--- movie_minds/src/Components/Login.js
+++ movie_minds/src/Components/Login.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 const Login = () => {
+  const [isSignInForm, setisSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setisSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Navbar />
-      <div className="absolute">
+      <div className="absolute ">
         <img
+          className="bg-gradient from-[#333]"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/42df4e1f-bef6-499e-87ff-c990584de314/5e7c383c-1f88-4983-b4da-06e14c0984ba/IN-en-20230904-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="login"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-700 rounded-lg text-base"
+          />
+        )}
         <input
           type="email"
           placeholder="Email Address"
-          className="p-2 my-4 w-full"
+          className="p-2 my-2 w-full bg-gray-700 rounded-lg text-base"
         />
         <input
           type="password"
-          placeholder="Email Address"
-          className="p-2 my-4 w-full"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-700 rounded-lg text-base"
         />
-        <button className="p-2 my-6 bg-red-700 w-full">Sign In</button>
+        <button className="p-2 my-6 bg-red-700 w-full rounded-lg text-base">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-2 text-sm cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to MovieMinds? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
--- movie_minds/src/Components/Navbar.js
+++ movie_minds/src/Components/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full'>
       {/* <img 
         className='w-44'
         src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJqVZ0gdHREsGU1GwHdUBnM1c3zI1qElOn9oicKQeuOMKL40vTKKj4wu0W5ZPKphZDh-M&usqp=CAU'
         alt="logo"/> */}
-        <p className='text-red-600 font-bold'>MovieMinds</p>
+        <p className='text-[#E50914] font-bold text-4xl'>MovieMinds</p>
     </div>
   )
 }
