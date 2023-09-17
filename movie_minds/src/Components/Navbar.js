2f116617b99cfde50c28d30f9de0b54a31225b92
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-09-17 12:03:46 +0530

    place auth user code in useffect

--- movie_minds/src/Components/Navbar.js
+++ movie_minds/src/Components/Navbar.js
@@ -1,57 +1,60 @@
-import React, { useEffect } from 'react'
-import { auth } from '../Utils/Firebase'
-import { signOut } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { onAuthStateChanged } from 'firebase/auth'
-import { addUser, removeUser } from '../Utils/userSlice'
+import React, { useEffect } from "react";
+import { auth } from "../Utils/Firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "../Utils/userSlice";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    Authuser()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  const Authuser = onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        // https://firebase.google.com/docs/reference/js/auth.user
         const { uid, email, displayName, photoURL } = user;
-        console.log('uid', user)
-
         // Store user data in user slice
-        dispatch(addUser({ uid, email, displayName, photoURL }))
-        navigate('/browse')
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+        navigate("/browse");
       } else {
         // User is signed out
         dispatch(removeUser());
-        navigate('/')
+        navigate("/");
       }
-  });
-  
-  const handleSignOut = () => {
-    signOut(auth).then(() => {})
-    .catch((err) => {
-        navigate('/error')
     });
-  }
+  }, []);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((err) => {
+        navigate("/error");
+      });
+  };
 
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
       {/* <img 
         className='w-44'
         src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJqVZ0gdHREsGU1GwHdUBnM1c3zI1qElOn9oicKQeuOMKL40vTKKj4wu0W5ZPKphZDh-M&usqp=CAU'
         alt="logo"/> */}
-        <p className='text-[#E50914] font-extrabold text-3xl'>MovieMinds</p>
-        {user && <div className='flex p-2'>
-          <img className='w-12 h-12 rounded-full' src={user?.photoURL} alt='user_icon'/>
-          <button onClick={handleSignOut} className='font-bold text-white'>(Sign Out)</button>
-        </div>}
+      <p className="text-[#E50914] font-extrabold text-3xl">MovieMinds</p>
+      {user && (
+        <div className="flex p-2">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={user?.photoURL}
+            alt="user_icon"
+          />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            (Sign Out)
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
