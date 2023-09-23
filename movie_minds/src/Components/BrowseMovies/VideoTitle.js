e85e03a8838bd12b40e0f5b52c5c89465520a324
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-09-23 12:15:06 +0530

    added play and info icon

--- movie_minds/src/Components/BrowseMovies/VideoTitle.js
+++ movie_minds/src/Components/BrowseMovies/VideoTitle.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import play from "../../Utils/Icons/play.png"
+import info from "../../Utils/Icons/info.png"
 
 const VideoTitle = ({ title, description }) => {
     return (
         <div className='w-full aspect-video pt-[15%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black'>
             <h1 className='text-5xl font-bold'>{title}</h1>
             <p className='py-6 text-lg w-1/3'>{description}</p>
-            <div className='col-12 my-4'>
-                <button className='bg-white text-black py-3 px-9 text-lg rounded-lg hover:bg-opacity-80'>
-                    ▶️ Play
+            <div className='flex col-12 my-4'>
+                <button className='flex bg-white text-black py-3 px-9 text-lg rounded-lg hover:bg-opacity-80'>
+                    <img className='w-5 h-5 my-1 mx-1' src={play} alt="play_icon"/>
+                    <span>Play</span>
                 </button>
-                <button className='mx-2 bg-gray-500 text-white py-3 px-9 text-lg bg-opacity-50 rounded-lg hover:bg-opacity-80'>
-                    ℹ️ More Info
+                <button className='flex mx-2 bg-gray-500 text-white py-3 px-9 text-lg bg-opacity-50 rounded-lg hover:bg-opacity-80'>
+                    <img className='w-5 h-5 my-1 mx-1 bg-opacity-50' src={info} alt="info_icon"/>
+                    <span>More Info</span>
                 </button>
             </div>
         </div>
