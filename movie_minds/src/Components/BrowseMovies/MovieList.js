1534ac4a36d21bce488467a28bf51922205ab367
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-09-24 20:09:07 +0530

    fix browse card css

--- movie_minds/src/Components/BrowseMovies/MovieList.js
+++ movie_minds/src/Components/BrowseMovies/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
     return (
         <div className='px-4 overflow-x-hidden'>
             <h1 className='text-3xl py-4 text-white'>{title}</h1>
-            <div className='flex hover:overflow-x-scroll'>
+            <div className='flex hover:overflow-x-scroll overflow-x-hidden'>
                 <div className='flex'>
                     {movies?.map((movie) => {
                         return <MovieCard key={movie.id} posterPath={movie.backdrop_path}/>
