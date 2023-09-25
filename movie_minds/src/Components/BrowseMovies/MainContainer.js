73338abe23d99066e714a3a0cb549e84ec404cc5
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-09-25 19:19:49 +0530

    added shimmer to main container

--- movie_minds/src/Components/BrowseMovies/MainContainer.js
+++ movie_minds/src/Components/BrowseMovies/MainContainer.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import VideoTitle from './VideoTitle';
 import VideoBackground from './VideoBackground';
+import Shimmer from '../Shimmer';
 
 const MainContainer = () => {
     const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
-    if (!movies) return;
+    if (!movies) return <Shimmer/>;
     const mainMovie = movies[0]
     const { original_title, overview, id } = mainMovie
 
---
+++ movie_minds/src/Components/Shimmer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="bg-black bg-opacity-90 m-2 md:m-4 p-2 md:p-4 rounded-md md:rounded-lg">
+        <div className="h-36 md:h-96 md:w-[95%] px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-4 md:mx-7"></div>
+        <div className="bg-black bg-opacity-90 flex m-2 md:m-4">
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md8:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+        </div>
+        <div className="bg-black bg-opacity-90 flex m-2 md:m-4">
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+        </div>
+        <div className="bg-black bg-opacity-90 flex m-2 md:m-4">
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+          <div className="w-36 h-24 md:h-32 md:w-48 px-2 md:px-4 py-2 md:py-4 md:pb-6 bg-gray-700 rounded-sm md:rounded-lg mx-2 md:mx-4"></div>
+        </div>
+    </div>
+  );
+};
+
+export default Shimmer;
