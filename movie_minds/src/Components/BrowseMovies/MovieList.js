a53ff43a4268acd044f4a3012c329bf38c003953
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-11-16 20:29:03 +0530

    fixed scroll for mobile view

--- movie_minds/src/Components/BrowseMovies/MovieList.js
+++ movie_minds/src/Components/BrowseMovies/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
     return (
         <div className='px-4 md:overflow-x-hidden'>
             <h1 className='text-xl md:text-3xl py-2 md:py-4 text-white font-bold'>{title}</h1>
-            <div className='flex hover:overflow-x-auto overflow-x-hidden'>
+            <div className='flex overflow-x-auto md:hover:overflow-x-auto md:overflow-x-hidden'>
                 <div className='flex'>
                     {movies?.map((movie) => {
                         return <MovieCard key={movie.id} posterPath={movie.backdrop_path} title={movie.title} movieData={movie} />
