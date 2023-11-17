aa784d1bb6386dc5dc796604da24f126a4e18494
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-11-17 18:55:12 +0530

    added cusror pointer to movie card

--- movie_minds/src/Components/BrowseMovies/MovieCard.js
+++ movie_minds/src/Components/BrowseMovies/MovieCard.js
@@ -12,7 +12,7 @@ const MovieCard = ({ posterPath, title, movieData }) => {
 
   return (
     <div className="w-36 md:w-48 pr-2 md:pr-4 py-4 md:pb-6" onClick={() => handleMovieClick(movieData.id)}>
-      <div className="relative">
+      <div className="relative cursor-pointer">
         <p className="absolute text-[10px] leading-3 md:text-sm text-white bg-gradient-to-b from-black w-full px-1 md:px-2 py-1">
           {title}
         </p>
